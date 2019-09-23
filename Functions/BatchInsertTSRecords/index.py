804f25ebf93efade7fc1dda282cf94cd28c8ea9e
SAHIL DADIA <sahil@pvcameras.com>
2019-09-23 12:09:14 +0000

    Update index.py

--- Functions/BatchInsertTSRecords/index.py
+++ Functions/BatchInsertTSRecords/index.py
@@ -1,60 +1,106 @@
-"""
-
-BatchInsertTSRecords:
-
-[Function Description Here]
-
-"""
-
-
-
-"""
-
-Outer Handler Area
-Any code specified here (outside of handler()) will only be executed upon container initialisation.
-To be used when you only want something to be executed once **not on every execution**:
-
- e.g. 
-    Importing libraries/modules
-    Service client connections
-    Database connections
-    etc
-
-"""
-
-# Module Imports
-import os
+import json
+import time
+from base64 import b64encode, b64decode
+from multiprocessing import Process, Pipe
+import dynamo_helper
+import logging
 import boto3
+import itertools
+import datetime
 
-# Resource connections
-dynamo_db_client = boto3.client('dynamodb')
+logging.basicConfig(
+    level=logging.INFO)
 
+dynamo_put_client = boto3.client('dynamodb')
+dynamodb_resource = boto3.resource("dynamodb")
+serializer = boto3.dynamodb.types.TypeSerializer()
 
-def handler(event,context):
+def convert_to_putItem_format(item):
+    return {
+        "PutRequest": {"Item": dynamo_helper.serialize_to_ddb(item, serializer)}
+    }
+
+def convert_to_dynamo_put_item_format(data):
     """
-    Inner Handler Area
-    Any code specified here (inside of handler()) will be executed upon every invocation.
-    To be used when you want something to be executed for every invocation:
-    
-    e.g. 
-    Application Logic
-    
-    Input:
-      Event:  Type Dict
-              Data inputted to the Lambda function upon invocation
-    
-      Context:    Type Context (Object)  
-                  Contains metadata regarding the current function invocation
-                  
-                  e.g. function_name, function_version, memory_limit_in_mb
-                  Methods:    get_remaining_time_in_missis()
-    
-                  For more info, see:
-                  https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html               
-    
-
-    Anything returned from this handler method, will be provided to the invocation source as invocation output
-
+    Converts the aggregate values to DynamoDB's client PutItemRequest format for
+    sending data in batches.
     """
 
-    return 'Hello World'
+    logging.info("Converting the data to dynamo Put Item Type")
+
+    # convert to put item_type
+    TS_values_as_put_item = list(
+        map(convert_to_putItem_format, data)
+    )
+    logging.debug(
+        "PutItem Formatted Data : {}".format((TS_values_as_put_item))
+    )
+    logging.info("Converting the data to dynamo Put Item Type...Done")
+
+    return TS_values_as_put_item
+
+
+def batch_write_tagged_data_to_DynamoDB(tagged_data, 
+                TSDynamo_tablename="pvcam-test-TripCalculation-TimeseriesTable-Daily-2019-09-06"):
+                    
+    TS_values_as_put_item = list(
+        map(convert_to_putItem_format, tagged_data)
+    )
+    print(TS_values_as_put_item)
+    
+    dynamo_putItems = convert_to_dynamo_put_item_format(aggregate_values)
+
+    for record  in tagged_data:
+        s=time.time()
+        response = dynamo_put_client.batch_write_item(RequestItems={TSDynamo_tablename: record})
+        e = time.time()
+        print("Time taken for batch write : {}".format(e-s))
+        
+    
+
+def extract_data_from_kinesis(event):
+    logging.info("Extracting data from Kinesis")
+
+    all_records = []
+    for r in event["Records"]:
+        data = b64decode(r["kinesis"]["data"]).decode("utf-8")
+        data = json.loads(data)
+        data = {**data, **data["gps"]}
+        del data["gps"]
+        print(data)
+        all_records.append(data)
+
+    logging.info("All records : {}".format((all_records)))
+
+    logging.info("Extracting data from Kinesis...Done")
+
+    return all_records
+
+def chunks(l, n=25):
+    # For item i in a range that is a length of l,
+    for i in range(0, len(l), n):
+        # Create an index range for l of n items:
+        yield l[i : i + n]
+
+
+
+
+def lambda_handler(event, context):
+    data = extract_data_from_kinesis(event)
+    dynamo_putItems = convert_to_dynamo_put_item_format(data)
+    print(dynamo_putItems)
+    
+    for batch in chunks(dynamo_putItems, 25):
+        response = dynamo_put_client.batch_write_item(
+            RequestItems={"pvcam-test-TripCalculation-TimeseriesTable-Daily-2019-09-06": batch}
+        )
+        logging.info(
+            "Response from updating metric in batch : {}".format(
+                response["ResponseMetadata"]["HTTPStatusCode"]
+            )
+        )
+
+    
+    
+    
+    return 'Done adding records to daynamo using ODM'
