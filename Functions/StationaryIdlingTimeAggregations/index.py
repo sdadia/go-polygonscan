c59e09a0c81687a65910b9749bc1543c37740957
sahilpvcameras <sahil@pvcameras.com>
2019-10-09 16:13:58 +0100

    Added function to convert data to ts from unix epoch

--- Functions/StationaryIdlingTimeAggregations/index.py
+++ Functions/StationaryIdlingTimeAggregations/index.py
@@ -2,12 +2,9 @@ import logging
 from collections import deque
 import pandas as pd
 from typing import List, Dict, Tuple
-from pprint import pprint, pformat
-import inspect
-import functools
-pd.set_option('display.width', 1000)
-pd.set_option('float_format', '{:.2f}'.format)
-pd.set_option('display.max_columns', 500)
+from pprint import pformat
+
+pd.set_option("float_format", "{:.2f}".format)
 
 logging.basicConfig(
     format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s",
@@ -37,31 +34,30 @@ def verify_valid_state_values(
 
 def convert_PTC_to_df(P, T, C):
     df = pd.DataFrame.from_dict({"P": P, "T": T, "C": C})
-    df['T'] = pd.to_datetime(df['T'],unit='s')
-    return  df
+    df["T"] = pd.to_datetime(df["T"], unit="s")
+    return df
 
 
-def find_time_location(new_time: int, T:List):
+def find_time_location(new_time: int, T: List):
     logger.debug("Function arguments are : \n{}".format(pformat(locals())))
-
 
     if new_time > T[-1]:
         logger.warning("Given time is greater than all timestamps")
         return (-1, "end")
     elif new_time == T[-1]:
         logger.warning("Repeated timestamp : {}".format(new_time))
-        return (None, 'repeat')
+        return (None, "repeat")
     elif new_time < T[0]:
         logger.warning("Given time is less than all timestamps")
         return (0, "start")
     elif new_time == T[0]:
         logger.warning("Repeated timestamp : {}".format(new_time))
-        return (None, 'repeat')
+        return (None, "repeat")
     else:
         for index in range(0, len(T) - 1):
             if new_time == T[index]:
                 logger.warning("Repeated timestamp : {}".format(new_time))
-                return (None, 'repeat')
+                return (None, "repeat")
             elif T[index] < new_time < T[index + 1]:
                 logger.info(
                     "Found new time {} is between {} and {}".format(
@@ -70,28 +66,20 @@ def find_time_location(new_time: int, T:List):
                 )
                 return (index, "between")
 
+
 def clean_up(T, C):
-    '''
+    """
     Specified the index to remove
-    '''
+    """
     index_to_remove = []
-    for index in range(len(T)-1):
-        if (C[index] == C[index+1])  and ((T[index+1]-T[index])<=60):
-            index_to_remove.append(index+1)
+    for index in range(len(T) - 1):
+        if (C[index] == C[index + 1]) and ((T[index + 1] - T[index]) <= 60):
+            index_to_remove.append(index + 1)
 
-    logger.warning('Going to remove following index from PTC : {}'.format(index_to_remove))
+    logger.warning(
+        "Going to remove following index from PTC : {}".format(index_to_remove)
+    )
     return sorted(index_to_remove, reverse=True)
-
-
-def multipop(yourlist, itemstopop):
-    result = []
-    itemstopop.sort()
-    itemstopop = itemstopop[::-1]
-    for x in itemstopop:
-        result.append(yourlist.pop(x))
-    return result
-
-
 
 
 def update_state_transitions(
@@ -106,7 +94,7 @@ def update_state_transitions(
     ----------
     data : list of tuples
         The data is the states we want to find the transitions for. The data must look like this
-        [ ("12:20", 1), ("12:23", 1), ("12:24", 0) .....]. Where the first part must be time.
+        [ ("12:20", 1), ("12:23", 1), ("12:24", 0) .....]. Where the first part must be in UNIX TIME.
         Second part must be the state value. Valid state values are 0, 1, "-"
     state_transition_dictionary : dictionary with the following format
         {
@@ -139,7 +127,11 @@ def update_state_transitions(
     C = deque(state_transition_dictionary["curr"])
 
     for d in data:
-        logger.debug("Current data point is : {}".format((pd.to_datetime(d[0], unit='s'), d[1])))
+        logger.debug(
+            "Current data point is : {}".format(
+                (pd.to_datetime(d[0], unit="s"), d[1])
+            )
+        )
         new_point_ts = d[0]
         new_point_state = d[1]
 
@@ -159,26 +151,24 @@ def update_state_transitions(
         index, loc = find_time_location(new_point_ts, T)
         print(index, loc)
 
-
         # start means before every point
         # P   T       C
         # -1  10:00   F        <- new point inserted
         # F   10:20   0
-        if loc == 'start':
-            print('updating start')
+        if loc == "start":
+            print("updating start")
             P.insert(0, -1)
             T.insert(0, new_point_ts)
             C.insert(0, new_point_state)
-            
-            # set the index+1 P equal to index C
-            P[index+1] = C[index]
 
-        
+            # set the index+1 P equal to index C
+            P[index + 1] = C[index]
+
         # P   T       C
-        # -1  10:00   F        
+        # -1  10:00   F
         # F   10:20   0        <- new point inserted
-        elif loc == 'end':
-            print('updating end')
+        elif loc == "end":
+            print("updating end")
             P.append(-1)
             T.append(new_point_ts)
             C.append(new_point_state)
@@ -186,31 +176,28 @@ def update_state_transitions(
             # set the index P equal to index-1 C
             P[-1] = C[-2]
 
-
         # P   T       C
-        # -1  10:18   F        
+        # -1  10:18   F
         # F   10:20   0        <- new point inserted
-        # 0   10:22   0        
-        elif loc == 'between':
-            print('updating between')
-            P.insert(index+1, -1)
-            T.insert(index+1, new_point_ts)
-            C.insert(index+1, new_point_state)
+        # 0   10:22   0
+        elif loc == "between":
+            print("updating between")
+            P.insert(index + 1, -1)
+            T.insert(index + 1, new_point_ts)
+            C.insert(index + 1, new_point_state)
             # print([pd.to_datetime(x, unit='s') for x in (T[index], T[index+1], T[index+2] )])
 
-
             # set the P value of record we just added to that of the above
-            P[index+1] = C[index]
+            P[index + 1] = C[index]
 
             # Set the P value of the record after the record  we just added to be equal to
             # the C value of the record we just added
-            P[index+2] = C[index+1]
+            P[index + 2] = C[index + 1]
 
-        
         # P   T       C
-        # -1  10:00   F        
+        # -1  10:00   F
         # F   10:20   0        <- same point repeated then just forget about it!
-        elif loc== 'repeat':
+        elif loc == "repeat":
             continue
 
         logger.debug(
@@ -223,9 +210,6 @@ def update_state_transitions(
         del P[idx]
         del T[idx]
         del C[idx]
-    logger.debug(
-        "PTC after cleanup : \n{}".format(convert_PTC_to_df(P, T, C))
-    )
+    logger.debug("PTC after cleanup : \n{}".format(convert_PTC_to_df(P, T, C)))
 
-
-    return {'prev': list(P), 'time': list(T), 'curr': list(C)}
+    return {"prev": list(P), "time": list(T), "curr": list(C)}
--- Tests/UnitTests/test_stationaryidlingtime.py
+++ Tests/UnitTests/test_stationaryidlingtime.py
@@ -1,15 +1,14 @@
-import json
 import ciso8601
 import random
+
 random.seed(1)
-import datetime
 import logging
 import os
 import sys
 import unittest
-from pprint import pformat, pprint
 
 
+# Function import to test
 sys.path.append(
     os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 )
@@ -26,51 +25,30 @@ logging.getLogger("Functions.StationaryIdlingTimeAggregations.index").setLevel(
 logger = logging.getLogger(__name__)
 logger.setLevel(os.environ.get("LOG_LEVEL", logging.INFO))
 
-import os
 
 def convert_date_to_timestamp_unix(data):
-    return [(ciso8601.parse_datetime(x[0]).timestamp(), x[1]) for x in data] 
+    ans = []
+
+    for date, status in data:
+        print(date, status)
+        ans.append(
+            # (datetime.datetime.strptime(date, DATE_FORMAT).timestamp(), status)
+            (ciso8601.parse_datetime(date).timestamp(), status)
+        )
+
+    return ans
+
+
+def string_time_to_unix_epoch(data):
+    ans = []
+    for date in data:
+        # ans.append(datetime.datetime.strptime(date, DATE_FORMAT).timestamp())
+        ans.append(ciso8601.parse_datetime(date).timestamp())
+
+    return ans
 
 
 class TestStationaryIdlingTimeAggregations(unittest.TestCase):
-    data_1 = [
-        ("2019-06-26 12:10:36", 0),
-        ("2019-06-26 12:11:36", 1),
-        ("2019-06-26 12:12:36", 1),
-        ("2019-06-26 12:13:36", 1),
-        ("2019-06-26 12:14:36", 0),
-        ("2019-06-26 12:15:36", 1),
-        ("2019-06-26 12:16:36", 1),
-        ("2019-06-26 12:17:36", 0),
-        ("2019-06-26 12:18:36", 0),
-        ("2019-06-26 12:19:36", 0),
-        ("2019-06-26 12:20:36", 1),
-        ("2019-06-26 12:21:36", 1),
-        ("2019-06-26 12:22:36", 1),
-    ]
-    data_1 = [(ciso8601.parse_datetime(x[0]).timestamp(), x[1]) for x in data_1]
-
-    expected_ans_data_1 = {
-        "prev": [-1, 0, 1, 0, 1, 0],
-        "time": [
-            "2019-06-26 12:10:36",
-            "2019-06-26 12:11:36",
-            "2019-06-26 12:14:36",
-            "2019-06-26 12:15:36",
-            "2019-06-26 12:17:36",
-            "2019-06-26 12:20:36",
-        ],
-        "curr": [0, 1, 0, 1, 0, 1],
-    }
-    expected_ans_data_1["time"] = [
-        ciso8601.parse_datetime(x).timestamp()
-        for x in expected_ans_data_1["time"]
-    ]
-
-    data_2 = [
-
-            ]
-
     def test_verify_valid_state_values_fail(self):
         input_state_values = [1, 2, 3, 4]
         valid_state_values = [-1, 1, 0]
@@ -90,6 +68,42 @@ class TestStationaryIdlingTimeAggregations(unittest.TestCase):
             1, verify_valid_state_values(input_state_values, valid_state_values)
         )
 
+
+class TestTransitions(unittest.TestCase):
+    data_1 = [
+        ("2019-06-26T12:10:36Z", 0),
+        ("2019-06-26T12:11:36Z", 1),
+        ("2019-06-26T12:12:36Z", 1),
+        ("2019-06-26T12:13:36Z", 1),
+        ("2019-06-26T12:14:36Z", 0),
+        ("2019-06-26T12:15:36Z", 1),
+        ("2019-06-26T12:16:36Z", 1),
+        ("2019-06-26T12:17:36Z", 0),
+        ("2019-06-26T12:18:36Z", 0),
+        ("2019-06-26T12:19:36Z", 0),
+        ("2019-06-26T12:20:36Z", 1),
+        ("2019-06-26T12:21:36Z", 1),
+        ("2019-06-26T12:22:36Z", 1),
+    ]
+    data_1 = convert_date_to_timestamp_unix(data_1)
+    print(data_1)
+
+    expected_ans_data_1 = {
+        "prev": [-1, 0, 1, 0, 1, 0],
+        "time": [
+            "2019-06-26T12:10:36Z",
+            "2019-06-26T12:11:36Z",
+            "2019-06-26T12:14:36Z",
+            "2019-06-26T12:15:36Z",
+            "2019-06-26T12:17:36Z",
+            "2019-06-26T12:20:36Z",
+        ],
+        "curr": [0, 1, 0, 1, 0, 1],
+    }
+    expected_ans_data_1["time"] = string_time_to_unix_epoch(
+        expected_ans_data_1["time"]
+    )
+
     def test_update_state_transitions_unequal_state_transition_dictionary_len(
         self
     ):
@@ -105,88 +119,99 @@ class TestStationaryIdlingTimeAggregations(unittest.TestCase):
         value = self.data_1[0:2]
         value.reverse()
         new_state_transition = update_state_transitions(
-              value,
+            value,
             state_transition_dictionary={"prev": [], "time": [], "curr": []},
         )
-        self.assertEqual(new_state_transition['prev'], self.expected_ans_data_1['prev'][:2])
-        self.assertEqual(new_state_transition['time'], self.expected_ans_data_1['time'][:2])
-        self.assertEqual(new_state_transition['curr'], self.expected_ans_data_1['curr'][:2])
-
+        self.assertEqual(
+            new_state_transition["prev"], self.expected_ans_data_1["prev"][:2]
+        )
+        self.assertEqual(
+            new_state_transition["time"], self.expected_ans_data_1["time"][:2]
+        )
+        self.assertEqual(
+            new_state_transition["curr"], self.expected_ans_data_1["curr"][:2]
+        )
 
         # all data
         value = self.data_1.copy()
         value.reverse()
         new_state_transition = update_state_transitions(
-              value,
-            new_state_transition,
+            value, new_state_transition
         )
-        self.assertEqual(new_state_transition['prev'], self.expected_ans_data_1['prev'])
-        self.assertEqual(new_state_transition['time'], self.expected_ans_data_1['time'])
-        self.assertEqual(new_state_transition['curr'], self.expected_ans_data_1['curr'])
-
-
+        self.assertEqual(
+            new_state_transition["prev"], self.expected_ans_data_1["prev"]
+        )
+        self.assertEqual(
+            new_state_transition["time"], self.expected_ans_data_1["time"]
+        )
+        self.assertEqual(
+            new_state_transition["curr"], self.expected_ans_data_1["curr"]
+        )
 
     def test_update_state_transitions_data_in_forward(self):
-
         value = self.data_1[0:2]
         new_state_transition = update_state_transitions(
-              value,
+            value,
             state_transition_dictionary={"prev": [], "time": [], "curr": []},
         )
         # some data
         value = self.data_1.copy()
         new_state_transition = update_state_transitions(
-              value,
+            value,
             # state_transition_dictionary={"prev": [], "time": [], "curr": []},
-            new_state_transition
+            new_state_transition,
         )
-        self.assertEqual(new_state_transition['prev'], self.expected_ans_data_1['prev'])
-        self.assertEqual(new_state_transition['time'], self.expected_ans_data_1['time'])
-        self.assertEqual(new_state_transition['curr'], self.expected_ans_data_1['curr'])
-
-
+        self.assertEqual(
+            new_state_transition["prev"], self.expected_ans_data_1["prev"]
+        )
+        self.assertEqual(
+            new_state_transition["time"], self.expected_ans_data_1["time"]
+        )
+        self.assertEqual(
+            new_state_transition["curr"], self.expected_ans_data_1["curr"]
+        )
 
     def test_update_state_transitions_data_shuffle(self):
-
         value = self.data_1[0:2]
         new_state_transition = update_state_transitions(
-              value,
+            value,
             state_transition_dictionary={"prev": [], "time": [], "curr": []},
         )
 
         value = self.data_1.copy()
         random.shuffle(value)
         new_state_transition = update_state_transitions(
-              value,
+            value,
             state_transition_dictionary={"prev": [], "time": [], "curr": []},
         )
-        self.assertEqual(new_state_transition['prev'], self.expected_ans_data_1['prev'])
-        self.assertEqual(new_state_transition['time'], self.expected_ans_data_1['time'])
-        self.assertEqual(new_state_transition['curr'], self.expected_ans_data_1['curr'])
-
-
-
+        self.assertEqual(
+            new_state_transition["prev"], self.expected_ans_data_1["prev"]
+        )
+        self.assertEqual(
+            new_state_transition["time"], self.expected_ans_data_1["time"]
+        )
+        self.assertEqual(
+            new_state_transition["curr"], self.expected_ans_data_1["curr"]
+        )
 
     def test_find_time_location(self):
         # test case 1 - time is in between
         T = self.expected_ans_data_1["time"]
-        new_time = ciso8601.parse_datetime("2019-06-26 12:13:36").timestamp()
+        new_time = ciso8601.parse_datetime("2019-06-26T12:13:06Z").timestamp()
         index_1, loc = find_time_location(new_time, T)
         self.assertEqual(index_1, 1)
         self.assertEqual(loc, "between")
 
-
         # test case 2 - time before everything
         T = self.expected_ans_data_1["time"]
-        new_time = ciso8601.parse_datetime("2019-06-26 12:09:36").timestamp()
+        new_time = ciso8601.parse_datetime("2019-06-26T12:09:36Z").timestamp()
         index_1, loc = find_time_location(new_time, T)
         self.assertEqual(index_1, 0)
         self.assertEqual(loc, "start")
 
-
         # test case 2 - time after everything
         T = self.expected_ans_data_1["time"]
-        new_time = ciso8601.parse_datetime("2019-06-26 12:39:36").timestamp()
+        new_time = ciso8601.parse_datetime("2019-06-26T12:39:36Z").timestamp()
         index_1, loc = find_time_location(new_time, T)
         self.assertEqual(index_1, -1)
         self.assertEqual(loc, "end")
@@ -199,8 +224,8 @@ class TestStationaryIdlingTimeAggregations(unittest.TestCase):
         self.assertEqual(loc, "repeat")
 
 
-
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(TestStationaryIdlingTimeAggregations))
+    suite.addTest(unittest.makeSuite(TestTransitions))
     return suite
