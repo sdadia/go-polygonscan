422012551e05fbf1dd21600b2c7085fd45cd78ac
sahilpvcameras <sahil@pvcameras.com>
2019-10-21 14:24:15 +0100

    Removed unwanted LOG module import in test trip file

--- Tests/UnitTests/test_trip_calculator.py
+++ Tests/UnitTests/test_trip_calculator.py
@@ -11,7 +11,6 @@ sys.path.append(
     os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 )
 
-logging.getLogger("m2").setLevel(logging.ERROR)
 
 logger = logging.getLogger()
 logger.setLevel(os.environ.get("LOG_LEVEL", logging.INFO))
