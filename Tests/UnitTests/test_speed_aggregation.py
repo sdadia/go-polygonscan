5409fa4723ec86952c658f61a6aea20c275862af
sahilpvcameras <sahil@pvcameras.com>
2019-10-22 09:33:44 +0100

    fixed issue with the number of data points not being equal to count

--- Tests/UnitTests/test_speed_aggregation.py
+++ Tests/UnitTests/test_speed_aggregation.py
@@ -1,11 +1,15 @@
 import logging
 import datetime
+from dateutil.tz import tzutc
+import mock
 import os
 import sys
 import unittest
 import json
 from pprint import pprint, pformat
 
+
+os.environ["localhost"] = "1"
 log = logging.getLogger(__name__)
 log.setLevel(os.environ.get("LOG_LEVEL", logging.INFO))
 
@@ -21,19 +25,113 @@ from Functions.SpeedAggregations.index import (
     find_unique_span_timestamp,
     format_event_data,
     update_data_in_dynamo_using_ODM,
-    DATETIME_FORMAT3,
 )
+
+
+from pvapps_odm.Schema.models import AggregationModel
+from pvapps_odm.ddbcon import Connection
+from pvapps_odm.ddbcon import dynamo_dbcon
 
 pprint(logging.log)
 
 
 class Test_speed_aggregation(unittest.TestCase):
-    @unittest.SkipTest
+    # @unittest.SkipTest
     def test_handler(self):
         with open("./sample_speed_aggregation_input_event.json") as f:
             event = json.load(f)
         logging.debug("Event is :\n{}".format(pformat(event)))
         handler(event, None)
+
+    # @mock.patch(
+    # "Functions.SpeedAggregations.index.update_data_in_dynamo_using_ODM"
+    # )
+    @mock.patch(
+        "Functions.SpeedAggregations.index.get_metric_data_from_dynamo_batch_from_ODM_table_read"
+    )
+    @mock.patch("Functions.SpeedAggregations.index.extract_data_from_kinesis")
+    def test_handler_mock(
+        self,
+        mock_extract_data_from_kinesis,
+        mock_get_data_from_ODM,
+        # mock_update_data_in_dyamo,
+    ):
+        # mock_update_data_in_dyamo.side_effect = [[], []]
+        mock_extract_data_from_kinesis.side_effect = [
+            [
+                {
+                    "spanId": "123",
+                    "speed": "0.28",
+                    "timestamp": "2019-05-22 10:45:05.154000",
+                },
+                {
+                    "spanId": "123",
+                    "speed": "0.83",
+                    "timestamp": "2019-05-22 10:45:06.154000",
+                },
+                {
+                    "spanId": "123",
+                    "speed": "0.52",
+                    "timestamp": "2019-05-22 10:45:07.154000",
+                },
+            ],
+            [
+                {
+                    "spanId": "123",
+                    "speed": "0.28",
+                    "timestamp": "2019-05-22 10:45:05.154000",
+                },
+                {
+                    "spanId": "123",
+                    "speed": "0.83",
+                    "timestamp": "2019-05-22 10:45:06.154000",
+                },
+                {
+                    "spanId": "123",
+                    "speed": "0.52",
+                    "timestamp": "2019-05-22 10:45:07.154000",
+                },
+            ],
+        ]
+        mock_get_data_from_ODM.side_effect = [
+            [],
+            [
+                {
+                    "count": 3,
+                    "spanId_metricname": "123_speed",
+                    "speed": 0.5433333333333332,
+                    "timestamp": datetime.datetime(
+                        2019, 5, 22, 10, 45, tzinfo=tzutc()
+                    ),
+                }
+            ],
+        ]
+        with open("./sample_speed_aggregation_input_event.json") as f:
+            event = json.load(f)
+        logging.debug("Event is :\n{}".format(pformat(event)))
+        handler(event, None)
+        handler(event, None)
+
+        if "localhost" in os.environ:
+            ddb_test = dynamo_dbcon(
+                AggregationModel, conn=Connection("http://localhost:8000")
+            )
+            ddb_test.connect()
+        else:
+            ddb_test = dynamo_dbcon(AggregationModel, conn=Connection())
+            ddb_test.connect()
+
+        response = ddb_test.session.query("123_speed", None)[0].attribute_values
+        print(response)
+        response["value"] = round(response["value"], 5)
+
+        expected_response = {
+            "count": 6,
+            "spanId_metricname": "123_speed",
+            "value": 0.54333,
+            "timestamp": datetime.datetime(2019, 5, 22, 10, 45, tzinfo=tzutc()),
+        }
+        self.assertEqual(response, expected_response)
 
     def test_extract_data_from_kinesis(self):
         logging.info("Testing extract data from kinesis")
@@ -115,49 +213,48 @@ class Test_speed_aggregation(unittest.TestCase):
         input_data = [
             {
                 "spanId": "123",
-                "timestamp": "2019-05-22 10:46:07.154000",
+                "timestamp": "2019-05-22 10:46:07.154000+00:00",
                 "speed": 120,
             },
             {
                 "spanId": "123",
-                "timestamp": "2019-05-22 10:45:07.154000",
+                "timestamp": "2019-05-22 10:45:07.154000+00:00",
                 "speed": 160,
             },
             {
                 "spanId": "122",
-                "timestamp": "2019-05-22 10:45:07.154000",
+                "timestamp": "2019-05-22 10:45:07.154000+00:00",
                 "speed": 280,
             },
         ]
 
         expected_output = [
             {
-                "timestamp": "2019-05-22 10:46:07.154000+00:00",
+                "timestamp": "2019-05-22 10:46:00+00:00",
                 "spanId_metricname": "123_speed",
                 "speed": 120.0,
                 "count": 1.0,
             },
             {
-                "timestamp": "2019-05-22 10:45:07.154000+00:00",
+                "timestamp": "2019-05-22 10:45:00+00:00",
                 "spanId_metricname": "123_speed",
                 "speed": 160.0,
                 "count": 1.0,
             },
             {
-                "timestamp": "2019-05-22 10:45:07.154000+00:00",
+                "timestamp": "2019-05-22 10:45:00+00:00",
                 "spanId_metricname": "122_speed",
                 "speed": 280.0,
                 "count": 1.0,
             },
         ]
         event_df = format_event_data(input_data)
-        # print(event_df)
 
         for e1, e2 in zip(
             expected_output, list(event_df.to_dict(orient="index").values())
         ):
             # print(e1, e2)
-            self.assertCountEqual(e1, e2)
+            self.assertEqual(e1, e2)
 
     def test_update_data_in_dynamo_using_ODM(self):
         data = [
@@ -202,6 +299,7 @@ class Test_speed_aggregation(unittest.TestCase):
             },
         ]
         ans = update_data_in_dynamo_using_ODM(data)
+        print(ans)
         ans = [x.attribute_values for x in ans]
         for e1, e2 in zip(expected_output, ans):
             e2["timestamp"] = str(e2["timestamp"])
