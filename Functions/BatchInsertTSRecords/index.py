f7d40c10bb93cc9cb69bdafdd3b2cdbf62deaeb2
unknown <daniel@pvcameras.com>
2020-07-02 11:53:01 +0100

    adding try except clause for invalid TS records being inserted

--- Functions/BatchInsertTSRecords/index.py
+++ Functions/BatchInsertTSRecords/index.py
@@ -67,25 +67,29 @@ def put_data_into_TS_dynamo_modelC(data):
 
     # gather the data for modelC
     for d in data:
-        data = {
-            "device_id": str(d["deviceId"]),
-            "span_id": str(d["spanId"]),
-            "tstime": ciso8601.parse_datetime(d["timestamp"]).timestamp(),
-            "timezone" : str(d["timezone"]),
-            "dst" : str(d['offset']),
-            "speed": str(d["speed"]),
-            "io": str(d["io"]),
-            "acc": str(d["acc"]),
-            "gps_coords": {
-                "lat": str(d["lat"]),
-                "lng": str(d["lng"]),
-                "gps_timestamp": str(d["gpsTime"]),
-                "gps_valid": str(d["status"]),
-                "course": str(d["course"]),
-            },
-        }
+        try:
+            data = {
+                "device_id": str(d["deviceId"]),
+                "span_id": str(d["spanId"]),
+                "tstime": ciso8601.parse_datetime(d["timestamp"]).timestamp(),
+                "timezone" : str(d["timezone"]),
+                "dst" : str(d['offset']),
+                "speed": str(d["speed"]),
+                "io": str(d["io"]),
+                "acc": str(d["acc"]),
+                "gps_coords": {
+                    "lat": str(d["lat"]),
+                    "lng": str(d["lng"]),
+                    "gps_timestamp": str(d["gpsTime"]),
+                    "gps_valid": str(d["status"]),
+                    "course": str(d["course"]),
+                },
+            }
 
-        data_as_model.append(TSModelC(**data))
+            data_as_model.append(TSModelC(**data))
+        except KeyError as e:
+            logger.error(f'ERROR: {e}')
+            logger.error(d)
 
     # de-duplicate the models
     # the model you are using must contain a hash and eq function
